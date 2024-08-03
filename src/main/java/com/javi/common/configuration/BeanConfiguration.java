cc73621688a9c2c9a57ea411ed01d45cd4e659a8
Javier Orfo <javierorfo@prontonmail.com>
2024-08-02 22:32:53 -0300

    auditory bean fixed, spring boot 3.3.2

--- src/main/java/com/javi/common/configuration/BeanConfiguration.java
+++ src/main/java/com/javi/common/configuration/BeanConfiguration.java
@@ -3,18 +3,15 @@ package com.javi.common.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 import com.javi.auditory.ApplicationAuditorAware;
-import com.javi.auditory.AuditoryConfiguration;
 
 @Configuration
-@ComponentScan(basePackages = "com.javi", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = AuditoryConfiguration.class))
+@ComponentScan(basePackages = "com.javi")
 @EnableMongoAuditing(auditorAwareRef = "auditorAware")
 public class BeanConfiguration {
-    
      @Bean
      public AuditorAware<String> auditorAware() {
          return new ApplicationAuditorAware();
